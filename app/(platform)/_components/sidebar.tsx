db33dd3276a717fae86f5748d72610bfc9ced54d
bourichi taha <bourichi.taha@gmail.com>
2024-04-30 04:01:49 +0100

    sidebar skeleton loading state

--- app/(platform)/_components/sidebar.tsx
+++ app/(platform)/_components/sidebar.tsx
@@ -37,7 +37,16 @@ const Sidebar = (props: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     )
   }
@@ -56,7 +65,7 @@ const Sidebar = (props: SidebarProps) => {
       </div>
       <Accordion type="multiple" defaultValue={defaultAccordionValue} className="space-y-2">
         {
-          userMemberships.data.map(({organization}) => (
+          userMemberships.data.map(({ organization }) => (
             <NavItem
               key={organization.id}
               isActive={activeOrganisation?.id === organization.id}
