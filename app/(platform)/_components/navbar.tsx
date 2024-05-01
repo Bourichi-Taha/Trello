74c698c489331f1d445b313bea2bde5ca9ad28e3
bourichi taha <bourichi.taha@gmail.com>
2024-05-01 05:38:16 +0100

    create functionality from navbar

--- app/(platform)/_components/navbar.tsx
+++ app/(platform)/_components/navbar.tsx
@@ -3,26 +3,31 @@ import { Button } from "@/components/ui/button"
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { Plus } from "lucide-react"
 import MobileSidebar from "./mobile-sidebar"
+import FormPopover from "@/components/form/form-popover"
 
 
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      <MobileSidebar/>
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button variant={"primary"} size={"sm"} className="rounded-sm hidden md:block h-auto py-1.5 px-2">
-          Create
-        </Button>
-        <Button variant={"primary"} size={"sm"} className="rounded-sm block md:hidden ">
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button variant={"primary"} size={"sm"} className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button variant={"primary"} size={"sm"} className="rounded-sm block md:hidden ">
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
-        <OrganizationSwitcher 
+        <OrganizationSwitcher
           hidePersonal
           afterCreateOrganizationUrl={"/organization/:id"}
           afterSelectOrganizationUrl={"/organization/:id"}
@@ -31,19 +36,19 @@ const Navbar = () => {
             elements: {
               rootBox: {
                 display: "flex",
-                justifyContent:"center",
+                justifyContent: "center",
                 alignItems: "center"
               }
             }
           }}
         />
-        <UserButton 
+        <UserButton
           afterSignOutUrl="/"
           appearance={{
             elements: {
               avatarBox: {
-                height:30,
-                width:30
+                height: 30,
+                width: 30
               }
             }
           }}
