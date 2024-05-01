7f9618b0ed0e344680bd049b8948d4e4925642b9
bourichi taha <bourichi.taha@gmail.com>
2024-05-01 05:53:02 +0100

    board list: fetched boards and added loading skeleton

--- app/(platform)/organisation/[organisationId]/page.tsx
+++ app/(platform)/organisation/[organisationId]/page.tsx
@@ -1,24 +1,27 @@
 import { Separator } from "@/components/ui/separator";
 import Info from "./_components/info";
 import BoardList from "./_components/board-list";
+import { Suspense } from "react";
 
 
 interface OrganisationIdPageProps {
-    params: {
-        organisationId:string;
-    }
+  params: {
+    organisationId: string;
+  }
 }
 
-const OrganisationIdPage = (props:OrganisationIdPageProps) => {
+const OrganisationIdPage = (props: OrganisationIdPageProps) => {
 
-    const {organisationId} = props.params;
+  const { organisationId } = props.params;
 
   return (
     <div className="w-full mb-20">
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />} >
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   )
