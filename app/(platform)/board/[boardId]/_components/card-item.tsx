c6850240762033c24e962c0968df2caafebe6ddd
bourichi taha <bourichi.taha@gmail.com>
2024-05-04 01:04:30 +0100

    card create w/server action & card item ui

--- app/(platform)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/board/[boardId]/_components/card-item.tsx
@@ -12,7 +12,7 @@ const CardItem = (props: CardItemProps ) => {
     const {card,index} = props;
 
   return (
-    <div className="trancate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+    <div role="button" className="trancate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
         {
             card.title
         }
